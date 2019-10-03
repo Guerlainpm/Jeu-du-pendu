f507007f790fe0197e8acc3838338be9a1c0ee84
Guerlainpm <46000981+Guerlainpm@users.noreply.github.com>
2019-10-03 16:15:40 +0200

    chiffre + bouton disable + placholder/caracter min

--- index.js
+++ index.js
@@ -4,29 +4,46 @@ var erreursRestante = 10;
 
 document.getElementById("chooseWord").addEventListener("click", function(event) {
   wordToFind = document.getElementById("inputValue").value.toUpperCase();
-  hiddenWord = "_".repeat(wordToFind.length);
-  event.preventDefault();
-  document.getElementById("title").innerHTML = "A toi de trouver !";
-  document.getElementsByTagName("form")[0].style.display = "none";
-  document.getElementById("searchWord").style.display = "flex";
-  document.getElementById("error").style.display = "block";
-  document.getElementById("buttons").style.display = "flex";
-  document.getElementById("searchWord").innerHTML = hiddenWord;
-  document.getElementById("numberErrorLeft").innerHTML = 10;
-  document.getElementById("reload").style.display = "block";
-  buttonGenerator();
+  if (wordToFind.length < 3) {
+    document.getElementById("inputValue").placeholder = "Tu doit au moins mettre trois caractère";
+    document.getElementById("inputValue").value = "";
+    document.getElementById("inputValue").classList.add("placeholderColor");
+    event.preventDefault();
+  } else {
+    hiddenWord = "_".repeat(wordToFind.length);
+    event.preventDefault();
+    document.getElementById("title").innerHTML = "A toi de trouver !";
+    document.getElementsByTagName("form")[0].style.display = "none";
+    document.getElementById("searchWord").style.display = "flex";
+    document.getElementById("searchWord").innerHTML = hiddenWord;
+    document.getElementById("error").style.display = "block";
+    document.getElementById("buttons").style.display = "flex";
+    document.getElementById("buttonChiffre").style.display = "flex";
+    document.getElementById("numberErrorLeft").innerHTML = 10;
+    document.getElementById("reload").style.display = "block";
+    buttonGenerator();
+  }
 });
 
 function buttonGenerator() {
   var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
+  var chiffre = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
   alphabet.forEach(letter => {
     var createButton = document.createElement("button");
     createButton.appendChild(document.createTextNode(letter));
     document.getElementById("buttons").appendChild(createButton);
     createButton.addEventListener("click", event => {
       checkAndReplace(event.target.innerText);
-      // createButton.setAttribute(".rightLetter");
+
+    })
+  })
+  chiffre.forEach(letter => {
+    var createButton = document.createElement("button");
+    createButton.appendChild(document.createTextNode(letter));
+    document.getElementById("buttonChiffre").appendChild(createButton);
+    createButton.addEventListener("click", event => {
+      checkAndReplace(event.target.innerText);
+      event.target.disabled = true;
     })
   })
   // for (letter of alphabet) {
@@ -44,14 +61,14 @@ function buttonGenerator() {
   // }
 }
 
+
 function checkAndReplace(letter) {
-
   var splitHidden = hiddenWord.split("");
-
   if (wordToFind.includes(letter)) {
     for (var i = 0; i <= wordToFind.length; i++) {
       if (wordToFind[i] == letter) {
         splitHidden[i] = letter;
+        event.target.style.backgroundColor = "rgb(22, 172, 4)";
       }
     }
   }
@@ -59,20 +76,24 @@ function checkAndReplace(letter) {
   if (!splitHidden.includes("_")) {
     document.getElementById("searchWord").style.display = "none";
     document.getElementById("buttons").style.display = "none";
+    document.getElementById("buttonChiffre").style.display = "none";
     document.getElementById("title").innerHTML = "Tu a trouver le mot " + '"' + wordToFind + '"' + " ." + "Bien jouer";
   }
   if (erreursRestante == 0) {
     document.getElementById("searchWord").style.display = "none";
     document.getElementById("buttons").style.display = "none";
-    document.getElementById("title").innerHTML = "Tu n'as plus d'erreur autoriser. Tu as perdu !";
+    document.getElementById("buttonChiffre").style.display = "none";
+    document.getElementById("title").innerHTML = "Tu as utiliser tout tes essaies. Tu as perdu !";
     document.getElementById("reload").style.display = "block";
-    document.getElementById("result").innerHTML = "Le mot a trouver était : " + wordToFind;
+    document.getElementById("result").innerHTML = "Le mot à trouver était : " + wordToFind;
   } else if (!wordToFind.includes(letter)) {
     erreursRestante--;
+    event.target.style.backgroundColor = "rgb(255, 78, 66)";
   }
   hiddenWord = splitHidden.join("");
   document.getElementById("searchWord").innerHTML = hiddenWord;
   document.getElementById("numberErrorLeft").innerHTML = erreursRestante;
+  event.target.disabled = true;
 }
 
 
